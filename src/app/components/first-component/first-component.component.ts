2cd10c3f2c2eb0bd220e02dadcaab2d25d9993be
Luiz Henrique <lhcontato2020@gmail.com>
2023-10-23 21:02:44 -0300

    components structure

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css'],
+})
+export class FirstComponentComponent {
+  ngOnChanges(): void {
+    console.log('ngOnChanges');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  variable = 1;
+
+  varChange() {
+    this.variable++;
+  }
+}
